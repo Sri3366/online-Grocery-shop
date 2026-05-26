b39f6de9fbc8dd3d016948199248979a04c700d8
Sri3366 <srivenkatchigurusetti3@gmail.com>
2026-05-26 13:42:37 +0530

    configure database settings adapter for cloud database integration

--- backend/settings.py
+++ backend/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
 
 
