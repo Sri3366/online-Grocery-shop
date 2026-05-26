3da76c95a32c94eefefa7693d5151e22f40ff6be
Sri3366 <srivenkatchigurusetti3@gmail.com>
2026-05-27 02:15:13 +0530

    database: force sync database table definitions using syncdb flags

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
-python manage.py migrate
+
+# 🟢 FORCE: Re-sync deployment app states across PostgreSQL
+python manage.py makemigrations groceryapp
+python manage.py migrate groceryapp --run-syncdb
